55d1a3a128679b0fb6fa675d11652087fc6035e6
janthony25 <junanthony.business@gmail.com>
2024-02-06 06:59:29 +1300

    Add project files.

---
+++ OOP-Auto-Implemented-Properties/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MyProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Auto-Implemented properties = shortcut when no additional logic is required in the property
+            //                               you do not have to define a field for a property.
+            //                               you only have to write get; and /or set; inside the property
+
+            Car car = new Car("Porche");
+            Console.WriteLine(car.Model);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Car
+    {
+        public string Model { get; set; }
+
+
+        public Car(string model)
+        {
+            this.Model = model;
+        }
+    }
+}
